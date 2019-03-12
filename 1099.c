f19fde7b7ab1b8632ca748b4080a064ca3f83070
srijond <srijonrahman00@gmail.com>
2019-03-12 10:32:41 +0600

    uri 1099 solution

---
+++ 1099.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    int a,b,sum=0,j;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        if(a>b){
+            int temp=a;
+               a=b;
+               b=temp;
+        }
+        if(a%2==0)a--;
+        a=a+2;
+
+        for(j=a;j<b;j+=2){
+            sum+=j;
+        }
+       ar[i]=sum;
+       sum=0;
+
+
+    }
+    for(i=0;i<n;i++){
+        printf("%d\n",ar[i]);
+    }
+
+
+    return 0;
+}
