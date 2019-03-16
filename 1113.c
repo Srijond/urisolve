eb722b4a14320e3ad39d0b60840f017284a0937d
srijond <srijonrahman00@gmail.com>
2019-03-16 23:33:06 +0600

    uri solution 1113

---
+++ 1113.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   while(1)
+   {
+       scanf("%d %d",&a,&b);
+       if(a==b){
+        break;
+       }
+       else{
+        if(a>b)printf("Decrescente\n");
+        else{
+            printf("Crescente\n");
+        }
+       }
+
+
+
+   }
+
+
+
+
+
+
+
+    return 0;
+}
